95753d6eab27bbdd3d03cbc652e766c962dbeaa8
subhadeepchakraborty <33681479+subhadeepchakraborty@users.noreply.github.com>
2020-04-25 20:50:21 +0530

    Add files via upload

---
+++ homepage.php
@@ -0,0 +1,306 @@
+<html>
+
+<head>
+<center>
+<style>
+body
+{
+  margin:2%;
+  background-color: lightblue;
+
+}
+#head
+{
+width: 80%;
+height: 40px;
+}
+#img1{
+  float:left;
+height:120px;
+width:45%;
+padding-bottom:40px;
+
+}
+
+
+#splash
+{
+width:440px;
+height:300px;
+align-items: center;
+
+
+}
+#call
+{
+width:380px;
+height:300px;
+align-items: center;
+}
+#image
+{
+  width:100%;
+  height:100%;
+
+
+}
+#righthead
+{
+width:parent;
+height:25px;
+}
+ul
+{
+list-style:none;
+margin: 0px;
+margin-top: 20px;
+}
+
+li
+{
+  padding: 12px;
+margin-top: 70px;
+display: inline-block;
+width:77px;
+height:20px;
+
+}
+#headdown
+{
+  height:600px;
+width: parent;
+margin:20px;
+}
+
+#down1
+{
+  height:600px;
+  width: parent;
+  font-size: 30px;
+
+  margin:20px;
+  color:blue;
+align:left;
+}
+#categories
+{
+  height:150px;
+  width: parent;
+  margin:20px;
+  font-weight: bold;
+
+  margin-top: 160px;
+  font-style: oblique;
+
+
+
+}
+
+#left
+{
+height:400px;
+width:50%;
+float:left;
+text-align: left;
+
+}
+
+#green:hover
+{
+background-color:#90ee90;
+
+}
+
+#right
+{
+height:400px;
+width:49%;
+float:left;
+
+}
+
+
+
+#in{
+height:100px;
+width:200px;
+border-radius: 50%;
+
+}
+p
+{
+  font-size: 25px;
+  align: left;
+}
+#inner
+{ margin-top: 10px;
+height:100%;
+width:19%;
+float:left;
+margin: 5px;
+}
+li:hover
+{
+background-color: #add8e6;
+color: white;
+text-decoration: underline;
+
+}
+#down
+{
+  height:200px;
+  width:parent;
+  margin: 20px;
+  border-left: 15px solid white;
+  border-bottom: 5px solid white;
+
+}
+#footer
+
+{
+  margin-top: 600px;
+  font-size: 20px;
+}
+#mar
+{
+height:200px;
+margin:10px;
+
+
+}
+#why
+{
+font-size: 28px;
+border: 2px solid white;
+height: 400px;
+margin: 5%;
+text-align: justify;
+
+}
+</style>
+
+<body  >
+  <div id="ma">
+  <div id="head">
+
+<div id="righthead">
+  <div id="img1">
+   <img   src="eventoo.png"/>
+</div>
+  <strong>
+<font size="4px"><ul id="ul1" >
+  <a href="http://localhost/subh/mail/homepage.php"><li>HOME</li></a>
+<a href="http://localhost/subh/search.php"><li>SEARCH</li></a>
+<a href="http://localhost/subh/feedback.php"><li>FEEDBACK</li></a>&nbsp&nbsp
+<a href="http://localhost/subh/about.php"><li>ABOUT</li></a>
+<a href="http://localhost/subh/login.php"><li>SIGN UP</li></a>
+<a href="http://localhost/subh/MESSAGE.php"><li>ADMIN </li></a>
+
+
+</ul>
+</strong>
+</font>
+</div>
+
+<br><br>
+<div id="headdown">
+  <img id="image" />
+</div>
+<div id="categories">
+<div id="inner"><img id="in" src="food.jfif" />
+<br> FOOD CELEBRATION</div>
+<div id="inner"><img id="in" src="sport.jfif" />
+<br> SPORTS FUNCTION</div>
+<div id="inner"><img id="in" src="dance.jfif" />
+<br>DANCE FUNCTIONS</div>
+<div id="inner"><img id="in" src="dj.jfif" />
+<br>DJ SOUNDS</div>
+<div id="inner"><img id="in" src="business.jfif" />
+<br> BUSINESS MEETINGS</div>
+
+
+</div>
+
+
+<div id="why"><center><font size="40px;">why we are??</center>  </font><br>
+  Webapp is application program that is stored on a remote server and delivered over internet through a browser interface. In computing
+  ,a web application or web app is a client-server computer program which the client includes to run in a web browser.<br><br>
+  Our project involves to have admin panel for event entry. User can also suggest event entry by passing it to admin and later admin can decide event should be added or not.
+   Likewise assurance security is to be given to client and admin by giving them register and login page etc.
+   <br><br> <center><font size="20px" color="blue">- by developers</center>  </font>
+</div>
+<br><br><br>
+<marquee width="1400px" direction="left" scrollamount="15" loop="yes" ><img src="1.jfif" id="mar">&nbsp<img src="2.jfif" id="mar"><img src="3.jfif" id="mar"><img src="5.jfif" id="mar"><img id="mar" src="4.jfif">
+<img src="6.jfif" id="mar"><img src="7.jfif" id="mar"><img src="8.jfif" id="mar"><img src="9.jfif" id="mar"><img id="mar" src="4.jfif">
+<img src="101.jfif" id="mar"><img src="102.jfif" id="mar"><img src="103.jfif" id="mar"><img src="104.jfif" id="mar">
+
+</marquee>
+<br><br><br><br>
+<div id="down1">
+<div id="down">
+
+<center><b><font size="15px"> GOVERNMENT</font><b></center>
+<br>
+<p>public sector organizations use EVENTO event management software to reduce manual taks,
+streamline administrative ,process while helping increase event attendance.</p></div>
+<div id="down">
+<center><b><font size="15px"> EDUCATION</font><b></center>
+<br>
+<p>EVENTO management software enables education provider to plan all types of events,from courses and workshops with few ateendes
+to conference with thousand of invites.</p></div>
+<div id="down">
+<center><b><font size="15px"> ASSOCIATION</font><b></center>
+<br>
+<p>Integrate membership management software to help associations and other membership
+based organization manage their events and membership in a single solution.</p></div>
+<div id="down">
+<center><b> <font size="15px">MEETING PLANNER</font><b></center>
+<br>
+<p>Become more efficiently and grow your business by automating event planning process to quickly plan simple
+or complex events on behalf of your clients.</p></div>
+<div id="down">
+<center><b> <font size="15px"> CORPORATE AND FINANCE SERVICES</font><b></center>
+<br>
+<p>Navigate every aspect of your event planning as simply and efficiently as possible with
+ EVENTO easy yo use event management software </p></div>
+
+</div>
+<div id="footer">
+DEVELOPERS :- 1) NIKHAT TAJ &nbsp&nbsp&nbsp
+                   2)SIMRAN KARMANI&nbsp&nbsp&nbsp
+                   3)SHREYA MISHRA&nbsp&nbsp&nbsp
+                   4)SUBHADEEP CHAKRABORTY&nbsp&nbsp&nbsp
+                   <br><br>
+
+CONTACT US:- 9834758738 ,+1XXXXX
+<br><br>
+
+  COPYRIGHT 2020 &
+2021
+
+
+
+
+
+</div>
+<script>
+var time =2000;
+var img=["10.jpg","11.jpg","12.jpg","13.jpg","14.jpg"];
+var i=0;
+function sub1()
+{
+var a=  document.getElementById('image');
+a.src=img[i];
+i++;
+    if(i>=img.length)
+    {
+
+      i=0;
+    }
+
+}
+setInterval(sub1,time);
+</script>
+</body>
+
+</html>
