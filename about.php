79876730d2add0e747f1708360bcba83eb051354
subhadeepchakraborty <33681479+subhadeepchakraborty@users.noreply.github.com>
2020-04-25 21:03:38 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<html>
+<link href=".css" rel="stylesheet">
+<head>
+
+</head>
+<style>
+h1
+{
+background-color:#1569C7;
+height: 150px;
+color: white;
+align-self: center;
+
+
+}
+
+
+#about
+{
+  font-weight: bold;
+
+margin-top: 50px;
+margin-left: 20px;
+padding: 15px;
+width:45%;
+height:auto;
+background-color: ;
+float:left;
+border: 0;
+color: white;
+border-right: 4px solid #1569C7;
+font-size: 25px;
+color: #1569C7;
+text-align: justify;
+}
+body
+{
+height:200px;
+font-family: Courier;
+
+
+}
+
+#terms
+{
+  font-weight: bold;
+  margin-top: 55px;
+  margin-left: 20px;
+  padding: 10px;
+  width:45%;
+  height:auto;
+  float:left;
+  border: 0;
+  color: #1569C7;
+
+
+  font-size: 25px;
+  text-align: justify;
+}
+#quote
+{
+height: 50px;
+width: 100%;
+font-size: 20px;
+}
+img
+{
+margin-left: 520px;
+
+}
+</style>
+<body >
+  <b><a style="float:left; font-size:30px; font-color:blue;" href="http://localhost/subh/mail/homepage.php">HOME</a></b><br>
+
+<div id="about">
+<center><h1>About Us</h1></center>
+I.&nbspAll of us are here because we love Art.In addition,what is better than
+attending an Art show happening around You!
+<br><br><br>
+II.&nbspThere are hundreds of events happening right now while you read this.
+Tracking all of them is technically impossible, even in this age of technology.
+So, why not be a part of a community that will keep each other updated of awesome
+events so we miss none of them?
+<br><br><br>
+III.&nbspUse those filters and attend a show.Let us know how you like it and come back for more.
+</div>
+<div id="terms">
+<center><h1>Terms &  Conditions</h1></center>
+I.&nbsp We are just a listing platform and are not associated with any artist(s),
+organiser(s) or any other entity in any way.<br><br>
+II. &nbsp We will not be responsible for any event cancellation,
+rescheduling or any mishap during the event at venue.<br><br>
+III.&nbsp We respect your privacy and hate spam so rest assured that your details
+would remain safe with us.<br>
+<br>
+Current Collaborators:
+For any suggestions,queries,submit an event or just to say Hi! write to us at csubh143@gmail.com.
+</div>
+
+<center><img width="1000px" height="250px" src="eventoo.png" /></center>
+<br><center><div id="quote"><b><p>Best Viewed <i>without</i> Internet Explorer</b></p></center>
+</div>
+
+
+
+</body>
+
+</html>
